77f52ac28c129c66ab069d87fe760259ef4319ef
Ryszard Rzepa <ryszard@Ryszards-MacBook-Pro.local>
2017-12-04 17:21:12 +0100

    init commit

---
+++ src/screens/CreateProject/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {RkButton} from 'react-native-ui-kitten';
+import { View } from 'react-native';
+
+class CreateProject extends Component {
+  render() {
+    return (
+      <View>
+        <RkButton>Click me!</RkButton>
+      </View>
+    )
+  }
+}
+
+export default CreateProject;
